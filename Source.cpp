7a76188b487891169ef70336503b3860d20b0b66
Duckcher <108275769+Duckcher@users.noreply.github.com>
2022-07-04 03:27:53 +0300

    add fix pblem whit two maximums

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,5 @@
 #pragma once
 #define _CTR_SECURE_NO_WARNINGS
-#include <Windows.h>
 #include <time.h>
 #include <iostream>
 using namespace std;
@@ -18,11 +17,10 @@ int inputInt(const char query[])
 }
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
 	srand(time(NULL));
 	int size = 0;
 	int* arr, i, max = 0, maxi;
+	int countofmax = 0;
 	while (size < 6)
 	{
 		size = inputInt("Input size: ");
@@ -41,23 +39,35 @@ int main()
 		{
 			cout << " " << arr[i] << " ";
 		}
+		cout << "" << endl;
 		for (i = 0; i < size; ++i)
 		{
 			if (arr[i] > max)
 			{
 				max = arr[i];
 				maxi = i;
+				countofmax += 1;
 			}
 		}
-		cout << "" << endl;
-		cout << "Number of maximum: " << maxi << endl;
-		for (i = 0; i <= 6; i++)
+		if (countofmax == 1)
 		{
 
+			cout << "" << endl;
+			cout << "Number of maximum: " << maxi << endl;
+			for (i = 0; i <= 6; i++)
+			{
+
+			}
+			swap(arr[maxi], arr[i]);
+			for (i = 0; i < size; ++i)
+			{
+				cout << " " << arr[i] << " ";
+			}
 		}
-		swap(arr[maxi], arr[i]);
-		for (i = 0; i < size; ++i)
+		else
 		{
-			cout << " " << arr[i] << " ";
+			cout << "We have two max elements" << endl;
+			system("pause");
+	
 		}
 }
